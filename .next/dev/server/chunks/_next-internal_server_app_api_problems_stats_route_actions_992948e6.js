a6ec6cf57530df7b53bee4883f8a4818f2dabae9
Arindam Hui <huiarindam6@gmail.com>
2026-03-02 19:18:22 +0530

    Fix: remove .next and node_modules from tracking

--- .next/dev/server/chunks/_next-internal_server_app_api_problems_stats_route_actions_992948e6.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/problems/stats/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_problems_stats_route_actions_992948e6.js.map
--- .next/dev/static/chunks/app_dashboard_page_tsx_e1cabf6d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7b27a396._.js",
-  "static/chunks/_72d6aa44._.js"
-],
-    source: "dynamic"
-});
